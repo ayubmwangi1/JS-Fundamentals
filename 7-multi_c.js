4652d9830f2f91cd0a0913323550030af089f7ef
ayubmwangi1 <ayubmwangi1@gmail.com>
2025-11-06 21:56:59 +0300

    Delete 7-multi_c.js

--- 7-multi_c.js
+++
@@ -1,7 +0,0 @@
-function printCisFun(x) {
-    if (!Number.isInteger(x)) {
-        console.log('Missing number of occurrences');
-        return;
-    }
-    for (let i = 0; i < x; i++) console.log('C is fun');
-}
