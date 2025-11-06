9a60d35bae705ebb91501383c34d737b05dd3d97
ayubmwangi1 <ayubmwangi1@gmail.com>
2025-11-06 21:49:43 +0300

    Delete 8-square.js

--- 8-square.js
+++
@@ -1,7 +0,0 @@
-function printSquare(size) {
-    if (!Number.isInteger(size)) {
-        console.log('Missing size');
-        return;
-    }
-    for (let i = 0; i < size; i++) console.log('X'.repeat(size));
-}
