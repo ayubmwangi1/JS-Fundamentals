c1a642ddc4c6c6476ab96861098ea4caeeb166ab
ayubmwangi1 <ayubmwangi1@gmail.com>
2025-11-06 15:34:50 +0300

    Add files via upload

---
+++ 2-arguments.js
@@ -0,0 +1,12 @@
+function checkArguments(args) {
+
+    if (args === 0) {
+        console.log("No argument");
+    } else if (args === 1) {
+        console.log("Argument found");
+    } else {
+        console.log("Arguments found");
+    }
+}
+
+checkArguments()
