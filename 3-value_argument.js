c69b66b6cd3fa82b1d59f602eb7ee64b7f8728fb
ayubmwangi1 <ayubmwangi1@gmail.com>
2025-11-06 22:11:56 +0300

    Delete 3-value_argument.js

--- 3-value_argument.js
+++
@@ -1,9 +0,0 @@
-function printFirstArgument(firstArg) {
-    // Check if firstArg is undefined.
-    // In JavaScript, omitted function arguments are automatically 'undefined'.
-    if (firstArg === undefined) {
-        console.log("No argument");
-    } else {
-        console.log(firstArg);
-    }
-}
