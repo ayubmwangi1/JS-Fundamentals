08a62b9adcf3146ccc28fe5f0067740959913632
ayubmwangi1 <ayubmwangi1@gmail.com>
2025-11-06 22:20:01 +0300

    Add files via upload

---
+++ 5-to_integer.js
@@ -0,0 +1,9 @@
+const [arg] = process.argv.slice(2);
+
+const number = parseInt(arg); //Parse interger is used to convert the argument to an interger or number method can also be used!
+
+if (isNaN(number)) {
+  console.log("Not a number");
+} else {
+  console.log(`My number: ${number}`);
+}
