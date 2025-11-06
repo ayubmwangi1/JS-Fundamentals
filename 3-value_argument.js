bd6f499a242b4b209fc843c101e53cf8e41ad5b3
ayubmwangi1 <ayubmwangi1@gmail.com>
2025-11-06 22:13:11 +0300

    Add files via upload

---
+++ 3-value_argument.js
@@ -0,0 +1,10 @@
+
+const [argumentOne, argumentTwo] = process.argv.slice(2);
+
+if (argumentOne === undefined) {
+  console.log("No argument");
+} else if (argumentTwo === undefined) {
+  console.log("HBTN");
+} else {
+  console.log("HBTN");
+}
