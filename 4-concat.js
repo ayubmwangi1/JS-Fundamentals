b78775f63eeffa7d0f2ea3b9f06a33399d5dc4eb
ayubmwangi1 <ayubmwangi1@gmail.com>
2025-11-06 22:16:49 +0300

    Add files via upload

---
+++ 4-concat.js
@@ -0,0 +1,9 @@
+const [argumentOne, argumentTwo] = process.argv.slice(2);
+
+if (argumentOne === undefined) {
+  console.log("undefined is undefined");
+} else if (argumentTwo === undefined) {
+  console.log("HBTN is undefined");
+} else {
+  console.log("Python is fun");
+}
