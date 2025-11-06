021dfb9c886da733ed1316ead62ed3e5a0f3dfb3
ayubmwangi1 <ayubmwangi1@gmail.com>
2025-11-06 17:46:06 +0300

    Add files via upload

---
+++ 9-add.js
@@ -0,0 +1,11 @@
+function add(a, b) {
+  const num1 = parseInt(a);
+  const num2 = parseInt(b);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    console.log('Please provide two valid integers.');
+    return;
+  }
+
+  console.log(num1 + num2);
+}
