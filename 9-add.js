5c0282b8506453a2b52f7a5c372289b703c487e8
ayubmwangi1 <ayubmwangi1@gmail.com>
2025-11-06 21:55:54 +0300

    Add files via upload

---
+++ 9-add.js
@@ -0,0 +1,13 @@
+
+const argOne = parseInt(process.argv[2]);
+const argTwo = parseInt(process.argv[3]);
+
+function add(a, b) {
+  return a + b;
+}
+
+if (isNaN(argOne) || isNaN(argTwo)) {
+  console.log("NaN");
+} else {
+  console.log(add(argOne, argTwo));
+}
