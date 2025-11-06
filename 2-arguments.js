b6ca8153566e95f9e08a5ef43560b1ed3d1da8c8
ayubmwangi1 <ayubmwangi1@gmail.com>
2025-11-06 16:27:05 +0300

    Add files via upload

---
+++ 2-arguments.js
@@ -0,0 +1,12 @@
+function argumentRest(...args) {
+    if (args.length === 0) {
+        console.log("No argument");
+    } else if(args.length === 1 ) {
+        console.log("Argument found");
+    }else
+    console.log("“Arguments found”")
+}
+
+argumentRest()
+argumentRest("best")
+argumentRest("best school")
